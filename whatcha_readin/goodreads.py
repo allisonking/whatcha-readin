3d4237d666c5c1b41f2194a3425acd67d720b821
Jimmy Zeng <jzengg@gmail.com>
2019-07-27 16:33:31 -0400

    move api key to env file

--- whatcha_readin/goodreads.py
+++ whatcha_readin/goodreads.py
@@ -1,28 +1,28 @@
+import os
+from typing import List
+
 import requests
 import xmltodict
 
-KEY = 'NTvl08balpMZU1socckSw'
-USER_ID = '20891766'
-SHELF = 'currently-reading'
+API_KEY = os.environ['GOODREADS_API_KEY']
+USER_ID = "20891766"
+SHELF = "currently-reading"
 VERSION = 2
 
-API_URL = 'https://www.goodreads.com/review/list'
+API_URL = "https://www.goodreads.com/review/list"
 
-params = {
-  'v': VERSION,
-  'shelf': SHELF,
-  'id': USER_ID,
-  'key': KEY
-}
-
-response = requests.get(API_URL, params)
-wrapped = xmltodict.parse(response.text)
-
-try:
-  books = wrapped['GoodreadsResponse']['reviews']['review']
-except KeyError:
-  books = []
+params = {"v": VERSION, "shelf": SHELF, "id": USER_ID, "key": API_KEY}
 
 
+def get_currently_reading() -> List[str]:
+    response = requests.get(API_URL, params)
+    wrapped = xmltodict.parse(response.text)
 
-print(books)
+    try:
+        reviews = wrapped["GoodreadsResponse"]["reviews"]["review"]
+        books = [r["book"] for r in reviews]
+        book_titles = [b["title"] for b in books]
+    except KeyError:
+        book_titles = []
+
+    return book_titles

4b0394b60a1e3016830b7c932853055b017484a9
Jimmy Zeng <jzengg@gmail.com>
2019-07-27 17:05:11 -0400

    add python-dotenv

---
+++ whatcha_readin/settings.py
@@ -0,0 +1,10 @@
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+
+def load_env():
+    # Create .env file path.
+    dotenv_path = join(dirname(__file__), ".env")
+
+    # Load file from the path.
+    load_dotenv(dotenv_path)

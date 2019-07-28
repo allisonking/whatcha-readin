ad3c391772588bc36fb1d98028e915bd069393a8
Allison King <allisonjuliaking@gmail.com>
2019-07-27 21:17:08 -0400

    fix status option of cli

--- whatcha_readin/cli.py
+++ whatcha_readin/cli.py
@@ -15,76 +15,67 @@ from whatcha_readin.utils import (
     get_git_root_dir,
     get_git_config_hooks_value,
 )
-
+from whatcha_readin.config import VERSION
 
 CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
-RESOURCES_DIR = os.path.join(CURRENT_DIR, 'resources')
-HOOK_FILENAME = 'commit_msg.py'
-HOOK_FILENAME_DEST = 'commit-msg'
+RESOURCES_DIR = os.path.join(CURRENT_DIR, "resources")
+HOOK_FILENAME = "commit_msg.py"
+HOOK_FILENAME_DEST = "commit-msg"
 
 
 @click.group()
-@click.version_option()
+@click.version_option(version=VERSION, prog_name="whatcha-readin")
 def cli():
     pass
 
 
 @cli.command(
-    name='status',
-    help='Shows whether whatcha-readin is installed for the current repository'
+    name="status", help="Shows whether whatcha-readin is activated for this repository"
 )
 def status():
     active = False
 
     git_root_dir = get_git_root_dir()
     if git_root_dir is not None:
-        local_hooks_path = os.path.join(git_root_dir, '.git', 'hooks')
-        hook_path = os.path.join(local_hooks_path, HOOK_FILENAME)
+        local_hooks_path = os.path.join(git_root_dir, ".git", "hooks")
+        hook_path = os.path.join(local_hooks_path, HOOK_FILENAME_DEST)
         if os.path.exists(hook_path):
-            _note('whatcha-readin is activated for the current repository!')
+            _note("whatcha-readin is activated for this repository!")
             active = True
 
     if not active:
-        _warn('whatcha-readin is not activated for the current repository')
+        _warn("whatcha-readin is not activated for this repository")
 
 
-@cli.command(
-    name='install',
-    help='Install whatcha-readin for current git repository',
-)
+@cli.command(name="install", help="Install whatcha-readin for current git repository")
 def local_install():
-    git_local_root = os.path.join(os.getcwd(), '.git')
-    local_hooks_path = os.path.join(git_local_root, 'hooks')
+    git_local_root = os.path.join(os.getcwd(), ".git")
+    local_hooks_path = os.path.join(git_local_root, "hooks")
     if os.path.exists(git_local_root) and os.path.isdir(git_local_root):
+        # copy our hook file over
         src_file = os.path.join(RESOURCES_DIR, HOOK_FILENAME)
         dst_file = os.path.join(local_hooks_path, HOOK_FILENAME_DEST)
         shutil.copyfile(src_file, dst_file)
         os.chmod(dst_file, 0o0775)
-        
-        #db_file = os.path.join(RESOURCES_DIR, DATABASE_FILE)
-        #db_link = os.path.join(local_hooks_path, DATABASE_FILE)
-        #force_symlink(db_file, db_link)
-        _note('Successfully installed for current repository!')
+        _note("Successfully installed for this repository!")
     else:
-        _warn('Not a git repository')
+        _warn("Not a git repository")
         sys.exit(1)
 
 
 @cli.command(
-    name='rm',
-    help='Uninstall whatcha-readin for current git repository',
+    name="uninstall", help="Uninstall whatcha-readin for current git repository"
 )
 def local_uninstall():
-    git_local_root = os.path.join(os.getcwd(), '.git')
-    hook_filepath = os.path.join(git_local_root, 'hooks', HOOK_FILENAME_DEST)
+    git_local_root = os.path.join(os.getcwd(), ".git")
+    hook_filepath = os.path.join(git_local_root, "hooks", HOOK_FILENAME_DEST)
     if os.path.exists(hook_filepath):
         os.remove(hook_filepath)
-        _note('Uninstalled for current repository')
+        _note("Uninstalled for current repository")
     else:
-        _warn('whatcha-readin is not installed for current repository!')
+        _warn("whatcha-readin is not installed for current repository!")
         sys.exit(1)
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli()

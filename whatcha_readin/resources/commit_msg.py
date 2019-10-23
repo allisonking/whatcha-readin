30e3e3faab8d3b4f75c8960923605a83b8080a6b
Allison King <allisonjuliaking@gmail.com>
2019-10-23 17:08:32 -0400

    append instead of opening file twice
    Books-reading:
    Steel Crow Saga (Steel Crow Saga, #1)
    The Trainable Cat: A Practical Guide to Making Life Happier for You and Your Cat

--- whatcha_readin/resources/commit_msg.py
+++ whatcha_readin/resources/commit_msg.py
@@ -5,13 +5,8 @@ from whatcha_readin.goodreads import get_currently_reading
 
 currently_reading = get_currently_reading()
 
-# get the original commit message
-with open(sys.argv[1]) as f:
-    msg = f.read()
-
-# append the currently reading books
-with open(sys.argv[1], "w") as f:
-    f.write(msg)
+# append the books to the existing commit message
+with open(sys.argv[1], "a") as f:
     if currently_reading:
         f.write("\nBooks-reading:")
     for book in currently_reading:
